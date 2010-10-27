20245c403f652c677ea7d7665cdc70db2ff4b397
Eric Davis <edavis@littlestreamsoftware.com>
2010-10-27 11:44:47 -0700

    Test that wiki's in alternative formats are blocked from bots

--- test/integration/bots_filter_test.rb
+++ test/integration/bots_filter_test.rb
@@ -6,13 +6,23 @@ class BotsFilterTest < ActionController::IntegrationTest
     should "block #{user_agent} from #{path}" do
       header "User-Agent", user_agent
       visit path
-      
+
       assert_response :forbidden
       assert_equal "Bots are not allowed to view this page.", response.body
     end
 
   end
-  
+
+  def self.should_not_block_bot(user_agent, path)
+    should "not block #{user_agent} from #{path}" do
+      header "User-Agent", user_agent
+      visit path
+
+      assert_response :success
+    end
+
+  end
+
   def self.should_block_bots(&path_proc)
     path = path_proc.call
 
@@ -25,6 +35,18 @@ class BotsFilterTest < ActionController::IntegrationTest
     should_block_bot('robot', path)
   end
 
+  def self.should_not_block_bots(&path_proc)
+    path = path_proc.call
+
+    should_not_block_bot('googlebot', path)
+    should_not_block_bot('yahoo! slurp', path)
+    should_not_block_bot('msnbot', path)
+    should_not_block_bot('baiduspider', path)
+    should_not_block_bot('yandex', path)
+    should_not_block_bot('spider', path)
+    should_not_block_bot('robot', path)
+  end
+
   def self.should_not_block_browsers(&path_proc)
     path = path_proc.call
 
@@ -35,7 +57,9 @@ class BotsFilterTest < ActionController::IntegrationTest
   end
 
   def setup
+    Role.anonymous.update_attribute(:permissions, Redmine::AccessControl.permissions.collect(&:name)) # All permissions for anon
     @project = Project.generate!(:is_public => true, :identifier => 'bots').reload
+    WikiPage.generate!(:wiki => @project.wiki, :title => 'Start', :content => WikiContent.new(:text => 'Hello'))
     @repository = Repository::Subversion.create(:url => "file:///#{ActiveSupport::TestCase.repository_path('subversion')}", :project => @project) do |repo|
       repo.type = 'Subversion'
     end
@@ -60,6 +84,16 @@ class BotsFilterTest < ActionController::IntegrationTest
 
     should_not_block_browsers { "/issues/calendar" }
     should_not_block_browsers { "/projects/bots/issues/calendar" }
+  end
+
+  context "wiki in default format" do
+    should_not_block_bots { "/projects/bots/wiki/Start" }
+    should_not_block_browsers { "/projects/bots/wiki/Start" }
+  end
+
+  context "wiki in txt format" do
+    should_block_bots { "/projects/bots/wiki/Start?format=txt" }
+    should_not_block_browsers { "/projects/bots/wiki/Start?format=txt" }
   end
   
 end
