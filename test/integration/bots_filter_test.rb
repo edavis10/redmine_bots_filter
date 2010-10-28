afea4809b2639f895756e74a063a46be54c6a5b5
Eric Davis <edavis@littlestreamsoftware.com>
2010-10-28 16:06:59 -0700

    Refactor: move shoulda helpers to the test_helper

--- test/integration/bots_filter_test.rb
+++ test/integration/bots_filter_test.rb
@@ -2,60 +2,6 @@ require 'test_helper'
 
 class BotsFilterTest < ActionController::IntegrationTest
 
-  def self.should_block_bot(user_agent, path)
-    should "block #{user_agent} from #{path}" do
-      header "User-Agent", user_agent
-      visit path
-
-      assert_response :forbidden
-      assert_equal "Bots are not allowed to view this page.", response.body
-    end
-
-  end
-
-  def self.should_not_block_bot(user_agent, path)
-    should "not block #{user_agent} from #{path}" do
-      header "User-Agent", user_agent
-      visit path
-
-      assert_response :success
-    end
-
-  end
-
-  def self.should_block_bots(&path_proc)
-    path = path_proc.call
-
-    should_block_bot('googlebot', path)
-    should_block_bot('yahoo! slurp', path)
-    should_block_bot('msnbot', path)
-    should_block_bot('baiduspider', path)
-    should_block_bot('yandex', path)
-    should_block_bot('spider', path)
-    should_block_bot('robot', path)
-  end
-
-  def self.should_not_block_bots(&path_proc)
-    path = path_proc.call
-
-    should_not_block_bot('googlebot', path)
-    should_not_block_bot('yahoo! slurp', path)
-    should_not_block_bot('msnbot', path)
-    should_not_block_bot('baiduspider', path)
-    should_not_block_bot('yandex', path)
-    should_not_block_bot('spider', path)
-    should_not_block_bot('robot', path)
-  end
-
-  def self.should_not_block_browsers(&path_proc)
-    path = path_proc.call
-
-    should "not block browsers from '#{path}'" do
-      visit path
-      assert_response :success
-    end
-  end
-
   def setup
     Role.anonymous.update_attribute(:permissions, Redmine::AccessControl.permissions.collect(&:name)) # All permissions for anon
     @project = Project.generate!(:is_public => true, :identifier => 'bots').reload
